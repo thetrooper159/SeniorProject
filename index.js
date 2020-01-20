0b7ac3c1e9b13ee77a7d52ee1839232ee4f7e1c5
Jeff9629 <36942502+Jeff9629@users.noreply.github.com>
2020-01-20 15:24:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*****************
 
-Controller For Family House 
+Controller For Family House
 
 *****************/
 
@@ -57,9 +57,8 @@ app.get('/', function(req, res) {
  res.render('home', {
 	});
  });
- 
- 
- 
+
+
  app.use(function(req, res, next){
     res.status(404);
     res.render('404');
@@ -73,5 +72,4 @@ app.use(function(err, req, res, next){
 app.listen(app.get('port'), function(){
  console.log( 'Express started on http://localhost:' +
  app.get('port') + '; press Ctrl-C to terminate.' );
-
 });
