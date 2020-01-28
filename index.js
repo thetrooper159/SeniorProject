da060f9b716c7bfe69da60af052f66753ff0dd57
Jeff9629 <36942502+Jeff9629@users.noreply.github.com>
2020-01-27 20:10:56 -0500

    added files

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /*****************
 
-Controller For Family House
+Controller For Family House 
 
 *****************/
 
 
-//Required package for project
+//Required packages for project
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var bodyParser = require('body-parser');
@@ -20,18 +20,24 @@ var validator = require('validator');
 
 
 
-//Required Files that give us credentials and other data
-//var modules = require('./lib/modules.js');
-//var login = require('./framework/login.js');
+
+/*Required Modules */
+var GLOBALS = require('./global_settings.js');
 
 
+/* Initializing App */
 var app = express();
 
+
+/* Adding locations for easier use within pages  */
 app.use(express.static(__dirname + '/assets'));
 app.use(express.static(__dirname + '/framework'));
 
+/* Initializing Cookie Parser */
 app.use(cookieParser());
 
+
+/* Setting up Session vars, will be evenutally moved to its own export */
 app.use(session({
 	secret: 'secret',
 	resave: true,
@@ -39,81 +45,87 @@ app.use(session({
 }));
 
 
+/* Setting Up bodparser inside of app */
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 app.use(express.urlencoded());
 
 
-
+/* Setting Up express-handlebars... A little different from handlebars will a few extra features that work well within express */
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 
+/* Specific to express-handlebars that features agressive caching, This is extremely effective and should be kept off while working on application  */
+//app.enable('view cache');
+
+
+
+/* Setting Up our global variables that can be used anywhere through the site. */
+app.locals.nav = GLOBALS.nav_items();
+
+
+/* Where we set the port for the app */
 app.set('port', process.env.PORT || 3000);
 
 
+
+/**********************
+
+Start of Routing Pages 
+
+***********************/
+
+/* Home Page */
 app.get('/', function(req, res) {
  res.render('home', {
+	
+	 });
+ });
+ 
+ /* Push Notifications Page */
+ app.get('/push', function(req, res) {
+ 	res.render('push', {
+	
+	 });
+ });
+ 
+ /* Linen Request */
+ 
+ app.get('/linen', function(req, res) {
+ res.render('linen', {
+	
+	 
 	});
  });
-
-app.get('/login', function(req, res) {
-res.render('login', {
- 	});
-});
-
-app.get('/about', function(req, res) {
-	res.render('about', {
-		});
-	 });
-
-app.get('/alert', function(req, res) {
-	res.render('alert', {
-		});
-	 });
-
-app.get('/FAQ', function(req, res) {
-	res.render('FAQ', {
-	 	});
-	 });
-
-app.get('/Linen', function(req, res) {
-	 res.render('Linen', {
-	 		});
-	 	 });
-
-//working code for previously used form
- app.post('/requ', function(req, res) {
-   var name = req.body.name;
-   var email = req.body.email;
-   var message = req.body.message;
-
-   var submit = {
-       name: req.body.name,
-       email: req.body.email,
-       message: req.body.message
-       }
-         var conn = mysql.createConnection(credentials.connection);
-         conn.query('INSERT INTO contact SET ?', submit, function(err, results, rows, fields) {
-           if (err) {
-             res.redirect(303, '/Linen?error='+err);
-           }else{
-               res.redirect('/Requested');
-             }
-           })
-         });
-
+ 
+ 
+ 
+ //*******KEEP ALL ROUTES ABOVE THIS ******************//
+ 
+ /* 404 Error Page */
  app.use(function(req, res, next){
     res.status(404);
     res.render('404');
 });
+
+/* 500 Error Page */
 app.use(function(err, req, res, next){
    console.log(err.stack);
     res.status(500);
     res.render('500');
 });
 
+
+/**********************
+
+Start of Routing Pages 
+
+***********************/
+
+/* Start Server */ 
 app.listen(app.get('port'), function(){
  console.log( 'Express started on http://localhost:' +
  app.get('port') + '; press Ctrl-C to terminate.' );
+
 });
