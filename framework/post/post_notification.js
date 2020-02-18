a0e182b7aba24c23aa382bdaf6c63539432c6fb0
Ib <ismirza@pointpark.edu>
2020-02-18 10:45:06 -0500

    cs

--- framework/post/post_notification.js
+++ framework/post/post_notification.js
@@ -4,25 +4,24 @@ var sql = require('../../settings.js');
 const connection = mysql.createConnection(sql);
 
 function insertNotification(notification, callback) {
-  connection.query('INSERT INTO notifications (guest, house, event, alert, date_time) VALUES (?, ?, ?, ?, ?, ?)',
-    [notification.title, notification.guest, notification.house, notification.event, notification.alert, notification.date_time],
-    function(err, headers, fields) {
+  connection.query('INSERT INTO notifications (guest, house, event, alert, date_time) VALUES (?, ?, ?, ?, ?)',
+    [notification.guest, notification.house,
+      notification.event, notification.alert, notification.date_time],
+    function (err, headers, fields) {
       if (err) {
-        data({error: err});
-      }
-      else{
+        console.log(err);
+      } else {
         callback();
       }
     });
 }
 
 insertNotification({
-  title: 'title',
   guest: 'guest',
   house: 'house',
   event: 'event',
   alert: 'alert',
   date_time: 'date-and-time',
-}, function() {
-  console.log("done!");
+}, function () {
+  console.log('done!');
 });
